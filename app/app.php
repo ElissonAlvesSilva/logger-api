fda6fc2c1cef42ed6f85f5d347dba150d1e10960
Elisson Alves Silva <elisson@MacBook-Pro-de-Elisson.local>
2019-09-08 10:31:24 -0400

    first commit

---
+++ app/app.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+require __DIR__ . '../../vendor/autoload.php';
+
+$settings = require 'config/settings.php';
+
+$baseDir = __DIR__ . '../';
+$envFile = $baseDir . '.env';
+if (file_exists($envFile)) {
+    $dotenv = Dotenv\Dotenv::create($baseDir);
+    $dotenv->load();
+}
+
+require 'initializer.php';
+require 'controllers.php';
+require __DIR__ . '/helpers/global.php';
+require __DIR__ . '/routes/web.php';
